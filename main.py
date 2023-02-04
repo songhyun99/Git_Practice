567afd766b4ab9b1cbd7a4a614b716ed50c27206
songhyun <songhyun0323@gmail.com>
2023-02-04 16:15:18 +0900

    feat: Update subtract function

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ num1 = int(input('Plz input num1: '))
 num2 = int(input('Plz input num2: '))
 
 print(num1 + num2)
+
+def subtract(): 
+    a= int(input('input num1 for sub:'))
+    b= int(input('input num2 for sub:'))
+    c=a-b
+    return c
+
+c=subtract()
+print('number returned is '+ str(c) )
